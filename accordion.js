cca08cff7e6495edd1de60aeb1f823893ee57c61
Dmitry <styx_00@mail.ru>
2022-08-09 01:17:56 +0700

    accordion

---
+++ accordion.js
@@ -0,0 +1,24 @@
+const accordion = (triggersSelector) => {
+     const btns = document.querySelectorAll(triggersSelector);
+     
+     btns.forEach(btn => {
+          btn.addEventListener('click', function() {
+               if (!this.classList.contains('active-style')) {
+                    btns.forEach(item => {
+                         item.classList.remove('active-style');
+                         item.nextElementSibling.classList.remove('active-content');
+                         item.nextElementSibling.style.maxHeight = "0px";
+                    });
+                    this.classList.add('active-style');
+                    this.nextElementSibling.classList.add('active-content');
+                    this.nextElementSibling.style.maxHeight = this.nextElementSibling.scrollHeight + 80 + "px"; 
+               } else {
+                    this.classList.remove('active-style');
+                    this.nextElementSibling.classList.remove('active-content');
+                    this.nextElementSibling.style.maxHeight = "0px";
+               }
+          });
+     });
+};
+
+export default accordion;
